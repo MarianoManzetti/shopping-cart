94e7581de186b941a47c5f51339a5efdc52b2fb0
MarianoManzetti <mariano.manzetti@gmail.com>
2025-08-11 21:37:42 -0300

    Feat: Add static product list

---
+++ data/products.js
@@ -0,0 +1,11 @@
+/**
+ * Static product list
+ * NOTE: In a real environment a database would be used
+ */
+
+export const products = [
+  { id: 1, name: "Producto 1", price: 60 },
+  { id: 2, name: "Producto 2", price: 100 },
+  { id: 3, name: "Producto 3", price: 120 },
+  { id: 4, name: "Producto 4", price: 70 }
+];
